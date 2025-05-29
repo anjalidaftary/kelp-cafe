01b088c214a8f19439993584d8f72642728c8ec6
anjalidaftary <anjali.daftary@gmail.com>
2025-05-29 09:49:19 -0400

    added coin system

---
+++ scenes/money.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@onready var clam_label: Label = $Label
+var clams = 50
+
+func add (num: int):
+	clams += num
+	clam_label.text = str(clams)
+
+func remove (num: int):
+	clams -= num;
+	clam_label.txt = str(clams)
+
+func balance () -> int:
+	return clams
+
+func canPurchase (num: int) -> bool:
+	return (num <=clams)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	clam_label.text = str(clams)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
