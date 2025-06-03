3b7c361213b5ac79cfa6d1bc488569577f67b7c3
anjalidaftary <anjali.daftary@gmail.com>
2025-06-03 09:44:49 -0400

    made topppings objects; added transition button

---
+++ items/topppings.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+class_name Topping
+# Called when the node enters the scene tree for the first time.
+@export var name: String = ""
+@export var order_symbol: Texture2D
+@export var base_station_symbol: Texture2D
