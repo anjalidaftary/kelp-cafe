d8d9f50b5b5e752101378358e8b9ab98c94c32d4
keya <keyachaurasia@gmail.com>
2025-06-08 12:48:58 -0400

    !

--- scenes/shop.gd
+++ scenes/shop.gd
@@ -1,11 +1,81 @@
 extends Control
+#@onready var shop = get_node("Shop")
+
+#@onready var deepbutton = shop.get_node("seasaucebutton")
+#@onready var seasaucecross = shop.get_node("SeaSauceCross")
+@onready var inkbutton = $inkbutton
+@onready var sandbutton = $sandbutton
+@onready var moonbutton = $moonbutton
+
+@onready var money = $Money
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var shop : Node = null
+var deepbutton : Node = null
+var seasaucecross : Node = null
+var inkcross : Node = null
+var mooncross : Node = null
+var sandcross : Node = null
+
+func _ready():
+	shop = get_node_or_null("/root/Game/Shop")
+	if shop:
+		deepbutton = shop.get_node_or_null("seasaucebutton")
+		seasaucecross = shop.get_node_or_null("SeaSauceCross")
+		inkbutton = shop.get_node_or_null("inkbutton")
+		inkcross = shop.get_node_or_null("InkCross")
+		moonbutton = shop.get_node_or_null("moonbutton")
+		mooncross = shop.get_node_or_null("MoonCross")
+		sandbutton = shop.get_node_or_null("sandbutton")
+		sandcross = shop.get_node_or_null("SandCross")
+	visible = false
+
 
 
 func _on_shop_button_pressed() -> void:
 	print("shown")
-	show() 
+	show()
+
+
+func _on_sea_sauce_button_pressed() -> void:
+	print("sea sauce button pressed")
+	money.show()
+	if money.canPurchase(50):
+		money.remove(50)
+		GlobalUnlockables.deepUnlocked = true
+		seasaucecross.show()
+		deepbutton.disabled = true
+	money.hide()
+
+
+func _on_ink_fudge_button_pressed() -> void:
+	print("ink button pressed")
+	money.show()
+	if money.canPurchase(50):
+		money.remove(50)
+		GlobalUnlockables.inkUnlocked = true
+		inkcross.show()
+		inkbutton.disabled = true
+	money.hide()
+
+
+func _on_sand_base_button_pressed() -> void:
+	print("sand button pressed")
+	money.show()
+	if money.canPurchase(50):
+		money.remove(50)
+		GlobalUnlockables.sandUnlocked = true
+		sandcross.show()
+		sandbutton.disabled = true
+	money.hide()
+
+
+func _on_jelly_base_button_pressed() -> void:
+	print("moon button pressed")
+	money.show()
+	if money.canPurchase(50):
+		money.remove(50)
+		GlobalUnlockables.moonJellyUnlocked = true
+		mooncross.show()
+		moonbutton.disabled = true
+	money.hide()
--- scenes/shop_layer.gd
+++ scenes/shop_layer.gd
@@ -1,12 +1,15 @@
-extends TextureButton
+extends CanvasLayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	visible = true
-	print("button instantiated") # Replace with function body.
+	visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_shop_button_pressed() -> void:
+	visible = true
