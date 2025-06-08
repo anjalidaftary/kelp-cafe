dfc50311202454e0fc96e089eb880c7cbe887470
anjalidaftary <anjali.daftary@gmail.com>
2025-06-08 09:59:14 -0400

    not to be dramatic but i wanna cry :(

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -7,20 +7,15 @@ extends Control
 @onready var sandcross = $SandCross
 @onready var inkcross = $InkCross
 @onready var mooncross = $MoonCross
-@onready var deepbutton = $deepPurchaseButton
-@onready var inkbutton = $inkPurchaseButton
+@onready var deepbutton = $seasaucebutton
+@onready var inkbutton = $inkbutton
+@onready var sandbutton = $sandbutton
+@onready var moonbutton = $moonbutton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hide()
 
-func _on_ink_purchase_button_pressed() -> void:
-	print("button pressed")
-	if money.canPurchase(50):
-		money.remove(50)
-		GlobalUnlockables.inkUnlocked = true
-		inkcross.show()
-		inkbutton.disabled = true
 
 func _on_seasaucebutton_pressed() -> void:
 	print("button pressed")
@@ -29,5 +24,36 @@ func _on_seasaucebutton_pressed() -> void:
 		money.remove(50)
 		GlobalUnlockables.deepUnlocked = true
 		seasaucecross.show()
-		deepbutton.disabled()
+		deepbutton.disabled = true
 	money.hide() # Replace with function body.
+
+
+func _on_sandbutton_pressed() -> void:
+	print("button pressed")
+	money.show()
+	if money.canPurchase(50):
+		money.remove(50)
+		GlobalUnlockables.sandUnlocked = true
+		sandcross.show()
+		sandbutton.disabled = true
+	money.hide()
+
+
+func _on_inkbutton_pressed() -> void:
+	print("button pressed")
+	if money.canPurchase(50):
+		money.remove(50)
+		GlobalUnlockables.inkUnlocked = true
+		inkcross.show()
+		inkbutton.disabled = true
+
+
+func _on_moonbutton_pressed() -> void:
+	print("button pressed")
+	money.show()
+	if money.canPurchase(50):
+		money.remove(50)
+		GlobalUnlockables.moonJellyUnlocked = true
+		mooncross.show()
+		moonbutton.disabled = true
+	money.hide()
