51b4d9cc8ad5ed7e1f930e43fa666f02f8648439
BellaLGuo <bellalguo@gmail.com>
2025-06-09 10:45:59 -0400

    fish anim starting

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -3,6 +3,7 @@ extends AnimatableBody2D
 @onready var anim_player = $PopUp
 @onready var speech_bubble = $"../SpeechBubble"
 @onready var squidAnim = $"../Squid/AnimationPlayer"
+@onready var sprite = $Sprite2D
 
 func _on_fish_button_2_pressed() -> void:
 	speech_bubble.show_order()
@@ -13,4 +14,6 @@ func _ready():
 	if (Global.currSit == 1):
 		anim_player.play("exit")
 		squidAnim.play("PopUp")
-	
+	if (Global.currSit == 2):
+		$Sprite2D.texture = load("res://assets/fish_dead.png")
+		anim_player.play("die")
