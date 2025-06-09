bc2928f76b5ecf0e6b2d1a278a5c3f5323a57551
fiobea26 <fsbeauchamp@gmail.com>
2025-06-09 08:08:00 -0400

    only order once

--- scripts/speech_bubble.gd
+++ scripts/speech_bubble.gd
@@ -31,33 +31,37 @@ var order
 	"caramel"
 ]
 
+@onready var gaveOrder = false
+
 func _on_ready() -> void:
 	hide()
 
 func show_order() -> void:
-	for child in symbol_container.get_children():
-		child.texture = null
-	
-	if(GlobalUnlockables.deepUnlocked):
-		random_toppings.append(deepsea)
-	if(GlobalUnlockables.inkUnlocked):
-		random_toppings.append(ink)
-	if(GlobalUnlockables.moonJellyUnlocked):
-		random_flavors.append(jelly)
-	if(GlobalUnlockables.sandUnlocked):
-		random_flavors.append(sand)
-	var first_symbol = random_flavors.pick_random()
-	var third_symbol = random_toppings.pick_random()
-	
-	symbol_container.get_child(0).texture = first_symbol.order_symbol
-	symbol_container.get_child(1).texture = foam_symbol.order_symbol
-	symbol_container.get_child(2).texture = third_symbol.order_symbol
-	var order: Array[Topping]  = [first_symbol, foam_symbol, third_symbol]
-	
-	Global.currOrder = order
-	
-	# storing order idk what this is but you need to fix it
-	#var index = random_symbols.find(third_symbol)
-	#order_success = [first_symbol, foam_symbol, random_symbols_string[index]]
-	
-	show()
+	if gaveOrder == false:
+		for child in symbol_container.get_children():
+			child.texture = null
+		
+		if(GlobalUnlockables.deepUnlocked):
+			random_toppings.append(deepsea)
+		if(GlobalUnlockables.inkUnlocked):
+			random_toppings.append(ink)
+		if(GlobalUnlockables.moonJellyUnlocked):
+			random_flavors.append(jelly)
+		if(GlobalUnlockables.sandUnlocked):
+			random_flavors.append(sand)
+		var first_symbol = random_flavors.pick_random()
+		var third_symbol = random_toppings.pick_random()
+		
+		symbol_container.get_child(0).texture = first_symbol.order_symbol
+		symbol_container.get_child(1).texture = foam_symbol.order_symbol
+		symbol_container.get_child(2).texture = third_symbol.order_symbol
+		var order: Array[Topping]  = [first_symbol, foam_symbol, third_symbol]
+		
+		Global.currOrder = order
+		
+		# storing order idk what this is but you need to fix it
+		#var index = random_symbols.find(third_symbol)
+		#order_success = [first_symbol, foam_symbol, random_symbols_string[index]]
+		
+		show()
+		gaveOrder = true
