c6c4b72b41d6b40e3cfdd840fb6d5e1eabe338ee
fiobea26 <fsbeauchamp@gmail.com>
2025-06-10 18:54:41 -0400

    works works works!!

--- scripts/counter.gd
+++ scripts/counter.gd
@@ -3,9 +3,9 @@ extends Node2D
 @onready var fish: AnimatableBody2D = $Fish
 @onready var squidPlayer = $Squid/AnimationPlayer
 @onready var fishPlayer = $Fish/PopUp
-@onready var toppings_node: Node2D = $ToppingsNode
 
 var customer
+var cup
 @onready var drink: Node2D = $FinishedOrder
 @onready var kelp: Sprite2D = $FinishedOrder/Kelp
 @onready var sand: Sprite2D = $FinishedOrder/Sand
@@ -22,47 +22,46 @@ func _ready() -> void:
 			fishPlayer.play("pop-up")
 			
 	if Global.currSit == 1 || Global.currSit == 2:
-		print("hello????")
 		drink.show()
 		if GlobalTopping.isKelp:
+			cup = kelp
 			kelp.show()
 			sand.hide()
 			jelly.hide()
 		elif GlobalTopping.isSand:
+			cup = sand
 			sand.show()
 			kelp.hide()
 			jelly.hide()
 		else:
+			cup = jelly
 			jelly.show()
 			kelp.hide()
 			sand.hide()
 			
 		if GlobalTopping.toppingsAdded.has("seafoam"):
 			var foamTexture = load("res://assets/sea-foam-topping.PNG")
-			add_topping(foamTexture, Vector2(43, -10))
+			add_topping(foamTexture)
 		if GlobalTopping.toppingsAdded.has("caramel"):
 			var caramelTexture = load("res://assets/caramel-sauce-topping.PNG")
-			add_topping(caramelTexture, Vector2(43, -10))
+			add_topping(caramelTexture)
 		if GlobalTopping.toppingsAdded.has("ink"):
 			var sauce = load("res://assets/ink-sauce-topping.png")
-			add_topping(sauce, Vector2(43, -10))
+			add_topping(sauce)
 		if GlobalTopping.toppingsAdded.has("deep"):
 			var sauce = load("res://assets/deep-sea-sauce-topping.png")
-			add_topping(sauce, Vector2(43, -10))
+			add_topping(sauce)
 		if GlobalTopping.toppingsAdded.has("pearl"):
 			var pearlTexture = load("res://assets/pearls-topping.PNG")
-			add_topping(pearlTexture, Vector2(43, -10))
+			add_topping(pearlTexture)
 		if GlobalTopping.toppingsAdded.has("seashell"):
 			var shellTexture = load("res://assets/seashells-topping.png")
-			add_topping(shellTexture, Vector2(43, -5))
+			add_topping(shellTexture)
+		await get_tree().create_timer(5.0).timeout
+		drink.hide()
 
-func add_topping(texture: Texture, offset := Vector2.ZERO):
-	if texture == null:
-		print("Error: texture is null, cannot create topping")
-		return
+func add_topping(texture: Texture):
 	var topping = Sprite2D.new()
 	topping.texture = texture
-	topping.position = offset
-	topping.scale = Vector2(0.50, 0.50)
-	toppings_node.add_child(topping)
-	toppings_node.move_child(topping, toppings_node.get_child_count() - 1)
+	cup.add_child(topping)
+	cup.move_child(topping, cup.get_child_count() - 1)
