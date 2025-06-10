52b238d9e433e59d8c314162114f128365bd327f
fiobea26 <fsbeauchamp@gmail.com>
2025-06-10 17:45:14 -0400

    money updates based on time now

--- scripts/arrow1.gd
+++ scripts/arrow1.gd
@@ -1,15 +1,23 @@
 extends Sprite2D
-
+@onready var timer_bar: Sprite2D = $"../TimerBar"
 
 func _on_change_scene_button_pressed() -> void:
-	if (GlobalTopping.orderSuccess()):
-		print("order is successful")
-		if(Timing.timeElapsed):
-			Global.currMoney += 50
-		elif(Timing.secElapsed):
-			Global.currMoney += int (Timing._getTime())
-		Global.currSit = 1
-	else:
+	if (!GlobalTopping.orderSuccess()):
 		print("order not successful")
 		Global.currSit = 2
+	else:
+		print("order is successful")
+		Global.currSit = 1
+		if timer_bar.zone == "green":
+			print("finished in green")
+			Global.currMoney += 50
+		elif timer_bar.zone == "yellow":
+			print("finished in yellow")
+			Global.currMoney += 30
+		elif timer_bar.position.x <= 394:
+			print("time ran out")
+			Global.currMoney += 0
+		else:
+			print("finished in red")
+			Global.currMoney += 10
 	get_tree().change_scene_to_file("res://scenes/Counter.tscn")
