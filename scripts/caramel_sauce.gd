2b112a4533539c1eb55ea2f35ac7013825b365ad
fiobea26 <fsbeauchamp@gmail.com>
2025-06-10 19:19:36 -0400

    Revert "oops..."
    This reverts commit f591bb525578254390df39e41c78e9b9c7a9b547.

--- scripts/caramel_sauce.gd
+++ scripts/caramel_sauce.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 
 var toggled = false;
 @onready var topping = $"."
-var cup
+@onready var drink = $"../Cup-kelp-milk"
 
 func _on_caramel_button_pressed() -> void:
 	if (toggled != true): 
@@ -16,6 +16,6 @@ func _on_drink_button_pressed() -> void:
 	if (toggled):
 		GlobalTopping.toppingsAdded.append("caramel")
 		var caramelTexture = load("res://assets/caramel-sauce-topping.PNG")
-		cup.add_topping(caramelTexture, Vector2(43, -10))
+		drink.add_topping(caramelTexture, Vector2(43, -10))
 		toggled = false;
 		topping.position.y += 50
